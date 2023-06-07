feb9b36bd27ef1a8870c7259e02db9d4a4eb6b96
facundoacostayl <facundoacostayl@outlook.com>
2023-06-07 06:37:00 -0300

    classes and logic created

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+    /*class Calculator {
+                    private double operand1;
+                    private double operand2;
+                    private string operator;
+
+                    //getters
+                    public double getOperand1() {return this.operand1}  
+                    public double getOperand2() {return this.operand2} 
+                    public string getOperator() {return this.operator} 
+
+                    //setters
+                    public void setOperand1(double operand1) {this.operand1 = operand1}  
+                    public void setOperand2(double operand2) {this.operand2 = operand2} 
+                    public void setOperator(string operator) {this.operator = operator} 
+
+                    public void calculate(operand1, operand2, operator)
+                    {
+                        ...logic
+                    }
+                }*/
+
+    /*class Calculator
+    {
+        private Stack<string> calculation;
+
+        //getters
+        public Stack<string> getCalculation() { return this.calculation; }
+
+        //setters
+        public void setCalculation(Stack<string> calculation) { this.calculation = calculation; }
+
+        public void calculate(double operand1, double operand2, string operatorVal)
+        {
+            ...logic
+                }
+    }*/
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        //POSSIBLE CLASES: CALCULATOR, SCREEN, NUMBERBUTTONS, OPERATIONBUTTONS
+
+
+        class Screen
+        {
+
+            //field
+            private double result;
+
+            public double getResult() { return this.result; }
+            public void setResult(double result) { this.result = result; }
+
+            //method
+            public void display()
+            {
+                Console.WriteLine("Result: " + this.result);
+            }
+
+        }
+
+        class NumberButton
+        {
+
+            //field
+            private double operand;
+
+            //getter
+            public double getOperand() { return this.operand; }
+
+            //setter
+            public void setOperand(double operand) { this.operand = operand; }
+
+        }
+
+        class OperatorButton
+        {
+
+            //field
+            private string operatorVal;
+
+              //getter
+              public string getOperatorVal() { return this.operatorVal; }
+
+            //setter
+            public void setOperatorVal(string operatorVal) { this.operatorVal = operatorVal; }
+
+        }
+
+        class Calculator
+        {
+            public double calculate(double operand1, double operand2, string operatorVal)
+            {
+                double result = 0;
+
+                switch (operatorVal)
+                {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+
+                    case "/":
+                        result = operand1 / operand2;
+                        break;
+                }
+
+                return result;
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---- POLISH CALCULATOR ----");
+            Console.WriteLine("---- FORMAT EXAMPLE: \"2 2 +\" OR \"47 7 *\" ----");
+            Console.WriteLine("\t");
+
+            //New Classes
+            NumberButton operand1 = new NumberButton();
+            NumberButton operand2 = new NumberButton();
+            OperatorButton operatorVal = new OperatorButton();
+            Calculator calculator = new Calculator();
+            Screen screen = new Screen();
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("\t");
+                    Console.WriteLine("Enter a calculation:");
+
+                    string s = Console.ReadLine();
+                    if (s == "") break;
+
+                    Stack<string> tks = new Stack<string>(s.Split(' '));
+
+                    if (tks.Count < 3 || Double.TryParse(tks.ElementAt(0), out double z))
+                    {
+                        throw new ArgumentException("Format Error");
+                    }
+
+                    if (!Double.TryParse(tks.ElementAt(2), out double x) || !Double.TryParse(tks.ElementAt(1), out double y))
+                    {
+                        throw new ArgumentException("Format Error");
+                    }
+
+                    operand1.setOperand(Convert.ToDouble(tks.ElementAt(2)));
+                    operand2.setOperand(Convert.ToDouble(tks.ElementAt(1)));
+                    operatorVal.setOperatorVal(tks.ElementAt(0));
+
+                    double result = calculator.calculate(operand1.getOperand(), operand2.getOperand(), operatorVal.getOperatorVal());
+
+                    screen.setResult(result);
+                    screen.display();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.Write(e.Message);
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
+
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        #region description
+        /*LEAPYEARS
+
+        Prior to 1582, the Julian Calendar was in wide use and defined leap years as every year divisible by 4.
+
+        However, it was found in the late 16th century that the calendar year had drifted from the solar year by
+
+        approximately 10 days. The Gregorian Calendar was defined in order to thin out the number of leap years
+
+        and to more closely align the calendar year with the solar year.
+
+        It was adopted in Papal countries on October 15, 1582, skipping 10 days from the Julian Calendar date.
+
+        Protestant countries adopted the Gregorian Calendar after some time.
+
+ 
+
+        User Story 1:
+
+ 
+
+       As a user, I want to know if a year is a leap year. So that I can plan for an extra day on February 29th
+
+        during those years.
+
+ 
+
+        Acceptance Criteria:
+
+ 
+
+        -Any year prior 1582 could be considered as NOT leap year.
+
+        -All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
+
+        -All years divisible by 100 but not by 400 are NOT leap years (for example, 1700, 1800, and 1900 were NOT leap years, NOR   will 2100 be a leap year),
+
+        -All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
+
+        -All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).
+
+         
+
+        Example:
+
+        Input: 2000 - Output: Leap
+
+        Input: 1997 - Output: Not Leap
+
+ 
+
+        Note: Consider valid input
+
+
+        --------------------------------------
+
+
+       Part 2
+
+       User Story 2:
+
+ 
+
+        As a user, I want to know the existing leap years given a year's range (from,to). 
+
+        In range, From must be <= than To, if not return Error
+
+      
+
+        Example
+
+        Input: 1999,2018 - Output: 5 (2000, 2004, 2008, 2012, 2016)
+
+        Input: 1997,1998 - Output: 0
+
+              
+        Input: 1998,1998 - Output: 0
+
+        Input: 2000,1990 - Output: Error
+
+        Input: 2000 - Output: Error
+
+ 
+
+        Note: Consider invalid input
+
+ 
+        */
+
+        #endregion
+
+        [TestMethod]
+        public void rpnCalculator()
+        {
+            
+        }
+    }
+}
