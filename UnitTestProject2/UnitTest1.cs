ffe27138e1f0fc8b0bebbf041ad44598c05f7126
facundoacostayl <facundoacostayl@outlook.com>
2023-06-28 08:32:23 -0300

    main classes and lists added

--- UnitTestProject2/UnitTest1.cs
+++ UnitTestProject2/UnitTest1.cs
@@ -1,106 +1,32 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Threading;
 
 namespace UnitTestProject2
 {
-    [TestClass]
-    public class UnitTest1
-    {
 
-        #region description
-        /*LEAPYEARS
+    #region
 
-        Prior to 1582, the Julian Calendar was in wide use and defined leap years as every year divisible by 4.
+    /*
+     
+     * Un banco, ofrece a sus clientes la posibilidad de ser titulares de una cuenta bancaria. 
+     * El titular de la cuenta debe tener nombre y apellido, y ser mayor de 18 años
+     * La cuenta bancaria puede ser de tipo caja de ahorro o de tipo cuenta corriente.
+     * La diferencia principal es que la cuenta corriente permite girar en descubierto hasta $5000 mientras que la caja de ahorro no es posible el giro en descubierto.
+     * Las operaciones pueden ser tanto de depósito como de extracción.
+     * Debe haber también un historial de los movimientos de cada cuenta, indicando fecha, hora, operación y monto.
 
-        However, it was found in the late 16th century that the calendar year had drifted from the solar year by
+     * De ser posible utilizar TDD/POO
+     
+    */
 
-        approximately 10 days. The Gregorian Calendar was defined in order to thin out the number of leap years
+    #endregion
 
-        and to more closely align the calendar year with the solar year.
-
-        It was adopted in Papal countries on October 15, 1582, skipping 10 days from the Julian Calendar date.
-
-        Protestant countries adopted the Gregorian Calendar after some time.
-
- 
-
-        User Story 1:
-
- 
-
-       As a user, I want to know if a year is a leap year. So that I can plan for an extra day on February 29th
-
-        during those years.
-
- 
-
-        Acceptance Criteria:
-
- 
-
-        -Any year prior 1582 could be considered as NOT leap year.
-
-        -All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
-
-        -All years divisible by 100 but not by 400 are NOT leap years (for example, 1700, 1800, and 1900 were NOT leap years, NOR   will 2100 be a leap year),
-
-        -All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
-
-        -All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).
-
-         
-
-        Example:
-
-        Input: 2000 - Output: Leap
-
-        Input: 1997 - Output: Not Leap
-
- 
-
-        Note: Consider valid input
-
-
-        --------------------------------------
-
-
-       Part 2
-
-       User Story 2:
-
- 
-
-        As a user, I want to know the existing leap years given a year's range (from,to). 
-
-        In range, From must be <= than To, if not return Error
-
-      
-
-        Example
-
-        Input: 1999,2018 - Output: 5 (2000, 2004, 2008, 2012, 2016)
-
-        Input: 1997,1998 - Output: 0
-
-              
-        Input: 1998,1998 - Output: 0
-
-        Input: 2000,1990 - Output: Error
-
-        Input: 2000 - Output: Error
-
- 
-
-        Note: Consider invalid input
-
- 
-        */
-
-        #endregion
-
-        [TestMethod]
-        public void rpnCalculator()
+    [TestMethod]
+        public void test()
         {
             
         }
